dabaedd222fbaf3a34984749a504d306d6d6fe08
dattatreya05 <89331208+dattatreya05@users.noreply.github.com>
2021-10-17 23:03:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<!-- 10. In your, HTML page add the below line and Highlight it without using any CSS.
+"HTML & CSS is awesome"  -->
+
+let button = document.getElementById("track");
+
+button.addEventListener("mouseover", () => {});
+
+let para = document.getElementById("para");
+para.addEventListener("mouseover", () => {
+    para.style.backgroundColor = "chartreuse";
+});
